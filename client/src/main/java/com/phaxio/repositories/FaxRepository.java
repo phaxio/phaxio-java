513f7b06885557e04ff033db712136b25e533eee
Noel Herrick <noelherrick@gmail.com>
2018-01-20 17:23:39 -0700

    Cleaning up streaming implementation and adding tests.

--- client/src/main/java/com/phaxio/repositories/FaxRepository.java
+++ client/src/main/java/com/phaxio/repositories/FaxRepository.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,12 +27,14 @@ public class FaxRepository {
      * Sends a fax
      * @param options A HashMap representing all the parameters for the fax. See the Phaxio documentation for the exact parameter names.
      *                Note that for fields that allow multiple items, use square brackets in the key and a Collection as the value.
-     *                Files can be added as File, file path or wrapped in new FileStream(inputStream, fileName).
+     *                Files can be added as File, a String file path or wrapped in new FileStream(inputStream, fileName).
      * @return The newly created Fax
      */
     public Fax create (Map<String, Object> options) {
         RestRequest request = new RestRequest();
         request.resource = "faxes";
+
+        ArrayList<Object> files = new ArrayList<Object>();
 
         for (Entry<String, Object> option : options.entrySet()) {
             if (option.getKey().equals("to[]")) {
@@ -45,14 +48,25 @@ public class FaxRepository {
                     request.addParameter(option.getKey(), url);
                 }
             } else if (option.getKey().equals("file")) {
-                addOneFile(option.getValue(), request, false);
+                files.add(option.getValue());
             } else if (option.getKey().equals("file[]")) {
-                Collection files =(Collection) option.getValue();
-                for (Object file : files) {
-                    addOneFile(file, request, true);
-                }
+                files.addAll((Collection)option.getValue());
             } else {
                 request.addParameter(option.getKey(), option.getValue());
+            }
+        }
+
+        boolean multipleFiles = files.size() > 1;
+
+        for (Object file : files) {
+            if (file instanceof File) {
+                addFile((File) file, request, multipleFiles);
+            } else if (file instanceof FileStream) {
+                addFile((FileStream) file, request, multipleFiles);
+            } else if (file instanceof String) {
+                addFile(new File((String) file), request, multipleFiles);
+            } else {
+                throw new IllegalArgumentException("file must be File, String (file path) or FileStream.");
             }
         }
 
@@ -121,24 +135,9 @@ public class FaxRepository {
         client.post(request);
     }
 
-    private void addOneFile(Object fileObject, RestRequest request, boolean multiple) {
-        if (fileObject instanceof File) {
-            addFile((File) fileObject, request, multiple);
-        } else if (fileObject instanceof FileStream) {
-            addFile((FileStream) fileObject, request, multiple);
-        } else if (fileObject instanceof String) {
-            addFile(new File((String) fileObject), request, multiple);
-        } else {
-            throw new IllegalArgumentException("file must be File, file path or FileStream.");
-        }
-    }
-
-    private void addFile (File file, RestRequest request, boolean multiple) {
-        String param = multiple ? "file[]" : "file";
-
+    private void addFile(File file, RestRequest request, boolean multiple) {
         try {
-            FileInputStream input = new FileInputStream(file);
-            addFileParam(param, file.getName(), input, request);
+            addFile(new FileStream(new FileInputStream(file), file.getName()), request, multiple);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -148,14 +147,10 @@ public class FaxRepository {
         String param = multiple ? "file[]" : "file";
         try {
             InputStream inputStream = fileStream.getInputStream();
-            addFileParam(param, fileStream.getFileName(), inputStream, request);
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            request.addFile(param, bytes, fileStream.getFileName(), null);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void addFileParam(String param, String file, InputStream input, RestRequest request) throws IOException {
-        byte[] bytes = IOUtils.toByteArray(input);
-        request.addFile(param, bytes, file, null);
     }
 }
